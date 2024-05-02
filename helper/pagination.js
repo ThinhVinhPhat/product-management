458df87113960df3c2cf78fe2fd6bbda32220c33
ThinhVinhPhat <120767551+ThinhVinhPhat@users.noreply.github.com>
2024-05-02 15:37:39 +0700

    update img

--- helper/pagination.js
+++ helper/pagination.js
@@ -5,7 +5,7 @@ module.exports = (pagination,query,countProduct) => {
 
     pagination.skip = (pagination.currentPage - 1) * pagination.limitofProduct
 
-    console.log(countProduct);
+   
     const totalPage = Math.ceil(countProduct/pagination.limitofProduct);
     pagination.totalPage = totalPage;
 
